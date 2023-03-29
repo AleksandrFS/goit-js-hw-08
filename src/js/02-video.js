942c27afbe5e75e9db6940eec5e8eaa6b8b3b929
AleksandrFedorov <ol.m.fed@gmail.com>
2023-03-29 16:44:20 +0300

    testHW_8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const data = function (data) {
+  {seconds: 0}
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(data, 1000));
+
+const timeBeforeFailure = Number(localStorage.getItem("videoplayer-current-time"));
+console.log(timeBeforeFailure);
+
+player
+  .setCurrentTime(timeBeforeFailure)
+//   .then(function (seconds) {
+    
+//   })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+
+
+
+
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_INPUT_KEY = 'feedback-form-state';
+
+const formRef = document.querySelector('.feedback-form');
+const textareaRef = document.querySelector('.feedback-form textarea');
+const inputRef = document.querySelector('.feedback-form input');
+
+formRef.addEventListener('input', throttle(onInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+messageInputBeforeFault();
+
+const allInputData = {};
+
+function onInput(e) {
+    // console.log(e.target.name)
+    // console.log(e.target.value);
+    allInputData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_INPUT_KEY, JSON.stringify(allInputData));
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    console.log(allInputData);
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_INPUT_KEY);
+}
+
+function messageInputBeforeFault() {
+
+    const savedMessageInput = localStorage.getItem(STORAGE_INPUT_KEY);
+    const parsedSavedMessageInput = JSON.parse(savedMessageInput);
+
+    if (parsedSavedMessageInput) {
+        textareaRef.value = parsedSavedMessageInput.message;
+        inputRef.value = parsedSavedMessageInput.email;
+    }
+}
